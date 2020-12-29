6cd0a1bff4d9666b21e497b54a7b32897e59fc61
Kaivalya <kaigauns13@gmail.com>
2020-12-29 10:51:36 +0530

    added proud members section which includes BNI and GTA in the home page, added girlscript as diversity partners in the footer, added role=button to burger menu, created the pop up for download portfolio in the home component and added more content to training page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,7 +11,28 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const pop = document.querySelector<HTMLElement>(".pop");
+    const download = document.querySelector<HTMLElement>(".download-js");
+    const downloadHero = document.querySelector<HTMLElement>(".download-hero-js");
+    const close = document.querySelector<HTMLElement>(".pop-close");
+    const background = document.querySelector<HTMLElement>(".popup-background-black");
+
+    download.addEventListener("click", openPop);
+    downloadHero.addEventListener("click", openPop);
+
+    function openPop() {
+      console.log("Hi")
+      pop.style.display = "block";
+      background.style.display = "block"; 
+    }
+
+    close.addEventListener("click", closePop);
+    background.addEventListener("click", closePop);
     
+    function closePop() {      
+      background.style.display = "none";
+      pop.style.display = "none";
+    }
   }
 
 }
